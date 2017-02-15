30b661965d27e32cd419c1b65c0856b6178a3278
Krzysztof Kosmider <kkusiu@gmail.com>
2017-02-15 16:50:30 +0100

    adding

--- std/array/ex1/main.cpp
+++ std/array/ex1/main.cpp
@@ -11,7 +11,7 @@ int main()
 
     array<int,4> a  = {4, 6, 78};
     array<int,2> a2 = {4, 6};
-    array<array<int,2>, 2> aa = { {4,5}, {4,5}  };
+    array<array<int,2>, 2> aa = {{ {4,5}, {4,5}  }};
 
     attay2D aa2 = { array1D{1,4}, array1D{1,4} };
 }
--- std/vector/ex1/main.cpp
+++ std/vector/ex1/main.cpp
@@ -1,10 +1,41 @@
 #include <iostream>
 #include <vector>
 
+using std::vector;
+class A
+{
+public:
+	A(){ std::cout << "A::A()" << std::endl; }
+	A(const A & a){ std::cout << "A::A(const A &)" << std::endl; }
+
+	A & operator=(const A & a)
+	{
+		std::cout << "A::operator=(const A & a)" << std::endl;
+	}
+
+
+std::vector<int> v;
+int* a;
+};
+
+
+
+
+
 int main()
 {
-    using std::vector;
-    
-    vector<int> v1();
+
+    A a;
+
+    vector<A> v2;
+    v2.push_back(a);
+
+	for(int i=0; i<100; i++)
+	{
+		v2.push_back(a);
+	}
+
+
+
     return 0;
 }
