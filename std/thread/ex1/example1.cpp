9f39d9d6134be85cf3d831f57ec6923fd29ffa0c
Krzysztof Kosmider <kkusiu@gmail.com>
2017-03-18 23:29:07 +0100

    Add something

--- std/thread/ex1/example1.cpp
+++ std/thread/ex1/example1.cpp
@@ -1,13 +1,25 @@
-#include <thread>
 #include <iostream>
-
-void my_thread_func()
+#include <thread>
+#include <algorithm>
+class WorkerThread
 {
-	std::cout << "My first thread..." << std::endl;
-}
-
-int main()
+public:
+    void operator()()     
+    {
+        std::cout<<"Worker Thread "<<std::this_thread::get_id()<<" is Executing"<<std::endl;
+    }
+};
+int main()  
 {
-	std::thread t(&my_thread_func);
-	t.join();
+    std::vector<std::thread> threadList;
+    for(int i = 0; i < 10; i++)
+    {
+        threadList.push_back( std::thread( WorkerThread() ) );
+    }
+    // Now wait for all the worker thread to finish i.e.
+    // Call join() function on each of the std::thread object
+    std::cout<<"wait for all the worker thread to finish"<<std::endl;
+    std::for_each(threadList.begin(),threadList.end(), std::mem_fn(&std::thread::join));
+    std::cout<<"Exiting from Main Thread"<<std::endl;
+    return 0;
 }
