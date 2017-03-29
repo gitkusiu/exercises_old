49c918ab57dde1b0ef42d1fe30b0b6a2809b3f86
Krzysztof Kosmider <kkusiu@gmail.com>
2017-03-29 05:51:07 +0200

    commit

--- std/vector/ex1/main.cpp
+++ std/vector/ex1/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <algorithm>
 
 using std::vector;
 class A
@@ -26,17 +28,35 @@ int main()
 {
 
     
-    A a;
+//    A a;
 
-    vector<A> v2;
-    v2.push_back(a);
+//    vector<A> v2;
+//    v2.push_back(a);
 
-	for(int i=0; i<100; i++)
-	{
-		v2.push_back(a);
-	}
+//	for(int i=0; i<100; i++)
+//	{
+//		v2.push_back(a);
+//	}
 
 
+    std::vector<std::string> v(10,"Yes");
 
+    for(std::string s : v)
+        std::cout << s + "\n"<< std::endl;
+    
+    std::vector<std::string>::iterator b = v.begin();
+    std::vector<std::string>::iterator e = v.end();
+
+    std::vector<int> v2;
+    for(int i=1; i<10; i++)
+        v2.push_back(i);
+
+
+   for(int i : v2)
+        std::cout << i << std::endl;
+        
+   std::vector<int>::iterator it = std::find(v2.begin(), v2.end(), 5);
+   
+   std::cout << std::endl << *it << std::endl;
     return 0;
 }
